7a40dafd2f754b43e0234a3ce1f88f9cceae1bdc
ChanKun <chan.nattawat@gmail.com>
2019-04-11 14:21:56 +0700

    Reivesed BBLCBProcessor

--- run_classifier.py
+++ run_classifier.py
@@ -399,16 +399,9 @@ class BBLCBProcessor(DataProcessor):
     """Creates examples for the training and dev sets."""
     examples = []
     for (i, line) in enumerate(lines):
-      # Only the test set has a header
-      if set_type == "test" and i == 0:
-        continue
       guid = "%s-%s" % (set_type, i)
-      if set_type == "test":
-        text_a = tokenization.convert_to_unicode(line[1])
-        label = "0"
-      else:
-        text_a = tokenization.convert_to_unicode(line[3])
-        label = tokenization.convert_to_unicode(line[1])
+      text_a = tokenization.convert_to_unicode(line[3])
+      label = tokenization.convert_to_unicode(line[1])
       examples.append(
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
